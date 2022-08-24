70721703f633d9ade4ecce78073a29795275f7c8
Devmc <dev.mcrf@qq.com>
2022-08-24 23:05:39 +0800

    profile

---
+++ src/views/login/props.js
@@ -0,0 +1,15 @@
+// sex options
+export const sexOptions = [
+  {
+    label: 'Unknown',
+    value: 0
+  },
+  {
+    label: 'Male',
+    value: 1
+  },
+  {
+    label: 'Female',
+    value: 2
+  }
+];
